b410fbd8d3c896a762ae8e394acea2aeb7cc1196
Choiwah.Chow <choiwah.chow@gmail.com>
2022-02-07 21:04:32 -0500

    minor update

--- gen.py
+++ gen.py
@@ -36,7 +36,7 @@ def run_algebra(id, params, outputs_dir):
 
         output_file_name = name.replace(" ", "_").replace("#", "").replace("-", "_") + f"_{order}.out"
 
-        cmd = f"./bin/mace4 -n{order} -N{order} -A1 -f {in_file} 2> {outputs_dir}/{output_file_name}"
+        cmd = f"./bin/mace4 -n{order} -N{order} -m-1 -A1 -f {in_file} 2> {outputs_dir}/{output_file_name}"
         print(cmd)
         cp = sp.run(cmd, capture_output=True, text=True, check=False, shell=True)
         cp = sp.run(f"tail -50  {outputs_dir}/{output_file_name} | grep interp | tail -1", capture_output=True, text=True, check=False, shell=True)
@@ -45,15 +45,13 @@ def run_algebra(id, params, outputs_dir):
 
 def run_all():
     all_algebras = read_algebras("algebras.xlsx")
-    os.makedirs("new_outputs", exist_ok = True)
+    os.makedirs("outputs", exist_ok = True)
     
     print(f'Started at {datetime.now()}')
     
     for id, params in all_algebras.items():
-        if id < 34:
-            continue
         print(datetime.now())
-        run_algebra(id, params, "new_outputs")
+        run_algebra(id, params, "outputs")
 
     print(f'Finished at {datetime.now()}')
 
